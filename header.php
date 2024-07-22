fc951aca09b559811ec44d0400dacb854e52f6b7
aki.code <renmt0910@gmail.com>
2024-07-22 10:38:47 +0900

    baseSetting,added header,img

--- header.php
+++ header.php
@@ -5,15 +5,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
     <?php wp_head(); ?>
-	        <!-- Google tag (gtag.js) -->
-      <script async src="https://www.googletagmanager.com/gtag/js?id=G-GPJT5E474W"></script>
-      <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
 
-        gtag('config', 'G-GPJT5E474W');
-      </script>
+    <!-- フォントファミリー -->
+    <script>
+    (function(d) {
+      var config = {
+        kitId: 'abv5hio',
+        scriptTimeout: 3000,
+        async: true
+      },
+      h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
+    })(document);
+    </script>
   </head>
   <body>
 
--- template/header-top.php
+++ template/header-top.php
@@ -1,18 +1,17 @@
 
 
   <header id="header" class="header">
-      <div class="inner header__inner">
+      <div class="header__inner">
         <h1 class="header__logo"><a href="<?php echo esc_url(home_url()); ?>"><?php bloginfo('name'); ?></a></h1>
         <nav class="header__nav">
           <ul class="header__list">
-            <li class="header__item"><a href="#works">WORKS</a></li>
-            <li class="header__item"><a href="#service">SERVICE</a></li>
-            <li class="header__item"><a href="#profile">PROFILE</a></li>
-            <li class="header__item"><a href="#contact">CONTACT</a></li>
+            <li class="header__item"><a href="#works">Works</a></li>
+            <li class="header__item"><a href="#service">Blog</a></li>
+            <li class="header__item"><a href="#contact"><span class="header__contact">Contact<span></a></li>
           </ul>
         </nav>
       </div>
-      <div class="drawer">
+      <!-- <div class="drawer">
         <span class="drawer__bar drawer__bar1"></span>
         <span class="drawer__bar drawer__bar2"></span>
         <span class="drawer__bar drawer__bar3"></span>
@@ -26,5 +25,5 @@
           <li class="drawer__item"><a href="#profile">PROFILE</a></li>
           <li class="drawer__item"><a href="#contact">CONTACT</a></li>
         </ul>
-      </nav>
+      </nav> -->
     </header>
