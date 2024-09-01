b4f067041c784b6bd2d0cb0b775008dc6d7a6b90
aki.code <renmt0910@gmail.com>
2024-09-01 21:12:01 +0900

    fixed pagenation

--- archive.php
+++ archive.php
@@ -6,15 +6,13 @@
         <article class="main__card">
             <div class="main__inner">
                 <h2 class="section__title">ブログ一覧</h2>
-
                 <?php if (have_posts() ) : ?>
                     <?php get_template_part('template/loop'); ?>
-                <?php
-                if(function_exists('pagination')) {
-                    pagination($wp_query->max_num_pages);
-                }
-                ?>
+
                 <?php endif; ?>
+				
+				<?php get_template_part('template/pagenavi'); ?>
+
             </div>
         </article>
         </main>
